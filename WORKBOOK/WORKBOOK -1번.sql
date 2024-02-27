f46ce184aa850b597e4863c824bbcf988bec86b4
DESKTOP-3GBEFFK\DREAMS03 <03dreams@naver.com>
2024-02-27 21:36:40 +0900

    250227_WORKBOOK -1번 완료

--- WORKBOOK/WORKBOOK -1번.sql
+++ WORKBOOK/WORKBOOK -1번.sql
@@ -11,21 +11,23 @@ SELECT DEPARTMENT_NAME || '의 정원은 '|| CAPACITY || '명 입니다.'
 FROM TB_DEPARTMENT;
 
 
--- 3번 * 
+-- 3번
 -- "국어국문학과" 에 다니는 여학생 중 현재 휴학중인 여학생을 조회하시오.
 -- (국문학과의 학과코드(DEPARTMENT_NO)는 001)
 SELECT STUDENT_NAME
 FROM TB_STUDENT
-WHERE DEPARTMENT_NO = '001' AND ABSENCE_YN = 'Y';
+WHERE DEPARTMENT_NO = '001' AND (ABSENCE_YN = 'Y' AND SUBSTR(STUDENT_SSN , 8, 1) = 2);
 
 
--- 4번 * 
+-- 4번
 -- 도서관에서 대출 도서 장기 연체자들을 찾아 이름을 게시하고자 한다.
 -- 그 대상자들의 학번이 다음과 같을 때 대상자들을 찾는 적절한 SQL구문을 작성하시오.
 -- A513079, A513090, A513091, A513110, A513119
 
-SELECT STUDENT_NAME 
-FROM TB_STUDENT;
+SELECT STUDENT_NAME
+FROM TB_STUDENT
+WHERE STUDENT_NO IN('A513079', 'A513090', 'A513091', 'A513110', 'A513119')
+ORDER BY STUDENT_NO DESC;
 
 
 -- 5번
